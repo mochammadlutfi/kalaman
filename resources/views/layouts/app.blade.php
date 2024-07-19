48f4fccece5ddd34173e9857ca5555edb83f8007
Mochammad Lutfi <mochammadlutfi125@gmail.com>
2024-07-20 04:28:35 +0700

    fix landing

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
 
-        <title>Admin | Kalaman</title>
+        <title>Admin | Kalaman Multimedia Karya</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>Kalaman Mutimedia Kreatif</title>
+        <title>Kalaman Mutimedia Karya</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
